bbec56e86259f1d98fbcb1024e937eabc1704d00
felldek <felldek1@gmail.com>
2021-10-29 00:55:59 +0300

    added possibility upload user avatar

--- src/ENVconfig.ts
+++ src/ENVconfig.ts
@@ -6,3 +6,5 @@ const development = {
   apiUrl: "http://localhost:3004/",
 };
 export const ENVconfig = process.env.NODE_ENV === "development" ? development : prodaction;
+
+export const PATH_TO_SRC = __dirname;
